359171b26e012682ce5724f57c0128b72a65ecb2
luchaoxin <chaoxin.lu@dreampix.cn>
2020-07-15 16:12:07 +0800

    去掉没用的代码

--- utils/sql_util.go
+++ utils/sql_util.go
@@ -2,7 +2,6 @@ package utils
 
 import (
 	"bytes"
-	"github.com/jinzhu/gorm"
 )
 
 func GetInCondition(args []int64) string {
@@ -21,7 +20,6 @@ func GetInCondition(args []int64) string {
 			buffer.WriteString(",")
 		}
 	}
-	db := gorm.DB{}
 	return buffer.String()
 }
 
